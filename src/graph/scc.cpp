84b09dbdf31687bcb56ccc5d53bf9175eb31d090
Antti Roeyskoe <aroysko@paivola.fi>
2019-01-07 05:19:56 +0200

    Rewrite strongly connected components

--- src/graph/scc.cpp
+++ src/graph/scc.cpp
@@ -1,75 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-
-struct Graph {
-	vector<vector<int>> conns;
-
-	void init(int n) {
-		conns.resize(n);
-	}
-	vector<int>& operator[](int i) {
-		return conns[i];
-	}
-	int size() const {
-		return conns.size();
-	}
-};
+typedef vector<vector<int>> Graph;
 
 // Struct for finding strongly connected components in a graph
-// Complexity: O(n + m log n)
+// Condensed graph is in reverse topological order
+// "comp" gives component in condensed graph that node i is in
+// Complexity: O(n + m)
 struct SCC {
-	Graph g; // Input graph
-	Graph rev_g; // g reversed
-	vector<int> topo; // Topological sort of g
-	vector<int> comp; // Component of node i in res
-	Graph res; // result graph
+	Graph g;
+	vector<int> comp;
+	vector<int> ind;
+	vector<int> sta;
+	int di = 0; // DFS counter
+	int cc = 0; // Comp count
 
-	void topoDfs(int i) {
-		if (comp[i]) return;
-		comp[i] = -1;
-		for (auto t : g[i]) topoDfs(t);
-		topo.push_back(i);
-	}
-	bool floodFill(int i, int v) {
-		if (comp[i] != -1) return false;
-		comp[i] = v;
-		for (auto t : rev_g[i]) floodFill(t, v);
-		return true;
-	}
-	void init(const Graph& graph) {
-		int n = graph.size();
-		g = graph;
+	int dfs(int i) {
+		if (ind[i] != -1) return (comp[i] == -1) ? ind[i] : di;
+		ind[i] = di;
+		int md = di;
+		++di;
 
-		// Build reverse graph
-		rev_g.init(n);
-		for (int i = 0; i < n; ++i) {
-			for (int t : g[i]) rev_g[t].push_back(i);
-		}
+		sta.push_back(i);
+		for (auto t : g[i]) md = min(md, dfs(t));
 
-		// Find topo order
-		comp.resize(n, 0);
-		for (int i = 0; i < n; ++i) topoDfs(i);
-		reverse(topo.begin(), topo.end());
-
-		// Find comps
-		int cc = 0;
-		for (int i : topo) cc += floodFill(i, cc);
-
-		// Build res graph
-		res.init(cc);
-		for (int i = 0; i < n; ++i) {	
-			for (auto t : g[i]) {
-				int ic = comp[i];
-				int tc = comp[t];
-				if (ic != tc) res[ic].push_back(tc);
+		if (md == ind[i]) {
+			while(comp[i] == -1) {
+				comp[sta.back()] = cc;
+				sta.pop_back();
 			}
+			++cc;
 		}
+		return md;
+	}
+	SCC(const Graph& gr) : g(gr), comp(gr.size(), -1), ind(gr.size(), -1) {
+		for (int i = 0; i < g.size(); ++i) dfs(i);
+	}
+	Graph getCond() {
+		Graph res(cc);
+		for (int i = 0; i < g.size(); ++i) {
+			for (auto t : g[i]) res[comp[i]].push_back(comp[t]);
+		}
+		vector<bool> seen(cc, false);
 		for (int i = 0; i < cc; ++i) {
-			sort(res[i].begin(), res[i].end());
-			res[i].erase(unique(res[i].begin(), res[i].end()), res[i].end());
+			seen[i] = true; // Remove self-loop
+			int j = 0; // Number of actuals
+			int r = 0; // Number of removes
+			while(j+r < res[i].size()) {
+				swap(res[i][j], res[i][j+r]);
+				if (seen[res[i][j]]) ++r;
+				else {
+					seen[res[i][j]] = true;
+					++j;
+				}
+			}
+			res[i].resize(j);
+			for (int v : res[i]) seen[v] = false;
+			seen[i] = false;
 		}
+		return res;
 	}
 };
 
@@ -77,7 +66,7 @@ struct SCC {
 int main() {
 	int n, m;
 	cin >> n >> m;
-	Graph g; g.init(n);
+	Graph g(n);
 	for (int j = 0; j < m; ++j) {
 		int a, b;
 		cin >> a >> b;
@@ -85,11 +74,11 @@ int main() {
 		g[a].push_back(b);
 	}
 
-	SCC scc; scc.init(g);
-
-	for (int i = 0; i < scc.res.size(); ++i) {
-		for (auto t : scc.res[i]) cout << i+1 << " -> " << t+1 << '\n';
+	SCC scc(g);
+	Graph res = scc.getCond();
+	for (int i = 0; i < res.size(); ++i) {
+		cout << i+1 << ": ";
+		for (auto t : res[i]) cout << t+1 << ' ' ; cout << '\n';
 	}
-	for (int i = 0; i < n; ++i) cout << scc.comp[i]+1 << ' '; cout << '\n';
-	for (int i = 0; i < n; ++i) cout << scc.topo[i]+1 << ' '; cout << '\n';
+	for (int i : scc.comp) cout << i+1 << ' '; cout << '\n';
 }
