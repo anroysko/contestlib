a6cf74236417b39ecb1eabe019b8c9a61f502112
Antti Roeyskoe <aroysko@paivola.fi>
2019-01-12 05:44:56 +0200

    change dir_mst to use shared pointer to avoid problems with pointer getting deleted by destructor

--- src/graph/dir_mst.cpp
+++ src/graph/dir_mst.cpp
@@ -2,123 +2,88 @@
 #include <utility>
 #include <vector>
 #include <queue>
+#include <memory>
 using namespace std;
 typedef long long ll;
 
-// Must be N*N*V < 2e19, to avoid overflows.
-// typedef __int128 ll for larger cases.
-const int N = 1e5; // Node count (used only for INF)
-const int V = 1e9; // Max edge cost
-const ll INF = (ll)N*V + 1;
-
 struct Edge {
-	int src;
-	int tar;
-	ll cost;
+	int s; // source
+	int t; // target
+	ll c; // cost
 };
-Edge makeEdge(int s, int t, ll c) {
-	Edge res;
-	res.src = s;
-	res.tar = t;
-	res.cost = c;
-	return res;
-}
 
 // Finds minimum directed spanning tree of input graph ( http://www.cs.tau.ac.il/~zwick/grad-algo-13/directed-mst.pdf )
 // Contracting runs in O(m log(n)), but expanding is O(n).
 // Therefore you can construct k MST's each with arbitrary root in O(m log n + kn)
 // To do this, make a copy of this data structure after calling contract.
 struct DirMST {
-	Edge* ed; // Edge data. Stored like this so copying is O(n)
-	int n, m; // Edge and node count
-	int cn; // Current node count
+	// MAX_NODES^2 * MAX_COST must not overflow!
+	const int MAX_NODES = 1e4; 
+	const int MAX_COST = 1e9;
+	const ll INF = (ll)MAX_NODES*MAX_COST + 1;
 
-	vector<int> ined; // in edge, Edge leading to this node
+	int n, cn; // Initial and current node count
+	vector<int> ined; // Edge leading to this node
 	vector<int> par; // Parent component
 	vector<int> group; // Highest-up supernode that contains this
-	vector<ll> base; // Base cost for edges into node. Added to all costs in "ins".
+	vector<ll> base; // Base cost for edges into node. Added to all costs in "ins"
 	vector<vector<int>> childs; // Child components
-	vector<priority_queue<pair<ll, int>>> ins; // All edges into this node, by cost.
+	vector<priority_queue<pair<ll, int>>> ins; // All edges into this node, by cost
+	shared_ptr<Edge[]> ed; // Edge data. Done like this for copying to work and be O(n).
 
-	DirMST() { ed = nullptr; }
-	~DirMST() { if (ed != nullptr) delete[] ed; }
-
-	void init(int nodes, const vector<Edge>& edges) {
-		n = nodes;
-		m = edges.size() + n;
-		ed = new Edge[m];
-		for (int i = 0; i < edges.size(); ++i) ed[i] = edges[i];
-		for (int i = 0; i < n; ++i) {
-			ed[i + edges.size()] = makeEdge(i, (i+1)%n, INF);
-		}
-		cn = n;
-
-		ined.resize(2*n);
-		par.resize(2*n);
-		group.resize(2*n);
-		base.resize(2*n);
-		childs.resize(2*n);
-		ins.resize(2*n);
-
-		for (int i = 0; i < 2*n; ++i) {
-			ined[i] = -1;
-			par[i] = i;
-			base[i] = 0;
-			group[i] = i;
-		}
-		for (int i = 0; i < m; ++i) {
-			ins[ed[i].tar].push({-ed[i].cost, i});
-		}
+	DirMST(int nn, const vector<Edge>& g) : n(nn), cn(nn), ined(2*nn, -1), par(2*nn), base(2*nn, 0),
+			childs(2*nn), ins(2*nn), ed(new Edge[nn+g.size()], default_delete<Edge[]>()) {
+		int m = n + g.size();
+		for (int i = 0; i < n; ++i) ed[i] = {i, (i+1)%n, INF};
+		for (int i = n; i < m; ++i) ed[i] = g[i-n];
+		for (int i = 0; i < m; ++i) ins[ed[i].t].emplace(-ed[i].c, i);
+		for (int i = 0; i < 2*n; ++i) par[i] = i;
+		group = par;
 	}
-
 	int coll(int i) {
 		if (group[i] != i) group[i] = coll(group[i]);
 		return group[i];
 	}
-
+	void joinQues(int a, int b) {
+		ll dif = base[b] - base[a];
+		if (ins[b].size() > ins[a].size()) {
+			dif = base[a] - base[b];
+			base[a] = base[b];
+			swap(ins[b], ins[a]);
+		}
+		while(! ins[b].empty()) {
+			auto el = ins[b].top();
+			ins[b].pop();
+			ins[a].emplace(el.first - dif, el.second);
+		}
+	}
 	void contract() {
-		int a = 0;
-		while(ins[a].size() != 0) {
+		int a = 0; // Active node
+		while(! ins[a].empty()) {
 			auto el = ins[a].top();
 			ins[a].pop();
-			base[a] = el.first;
-			// base[a] += el.first - base[a];
 			int ei = el.second;
 
-			int b = coll(ed[ei].src);
+			int b = coll(ed[ei].s);
 			if (a == b) continue;
 
 			ined[a] = ei;
-			if (ined[b] == -1) {
-				a = b;
-			} else {
+			base[a] = el.first;
+			if (ined[b] == -1) a = b;
+			else {
+				// Contract the cycle
 				a = cn;
 				++cn;
-				for (int j = b;;) {
-					par[j] = a;
-					group[j] = a;
-					childs[a].push_back(j);
-					
-					// Combine smaller into larger
-					ll dif = base[j] - base[a];
-					if (ins[j].size() > ins[a].size()) {
-						dif = base[a] - base[j];
-						base[a] = base[j];
-						swap(ins[j], ins[a]);
-					}
-					while(! ins[j].empty()) {
-						auto el = ins[j].top();
-						ins[j].pop();
-						ins[a].push({el.first - dif, el.second});
-					}
-
-					j = coll(ed[ined[j]].src);
-					if (j == a) break;
-				}
+				do {
+					par[b] = a;
+					group[b] = a;
+					childs[a].push_back(b);
+					joinQues(a, b);
+					b = coll(ed[ined[b]].s);
+				} while(b != a);
 			}
 		}
 	}
-
 	void dismantle(int r, vector<int>& vec) {
 		while(par[r] != r) {
 			for (auto c : childs[par[r]]) {
@@ -142,17 +107,16 @@ struct DirMST {
 		dismantle(r, vec);
 		for (int i = 0; i < vec.size(); ++i) {
 			int ei = ined[vec[i]];
-			int t = ed[ei].tar;
+			int t = ed[ei].t;
 			ined[t] = ei;
 			dismantle(t, vec);
 		}
 
-		vector<int> res (n-1);
+		vector<int> res;
 		for (int i = 0; i < n; ++i) {
-			if (i != r) {
-				if (ined[i] > m-n) return {};
-				res[i - (i > r)] = ined[i];
-			}
+			if (i == r) continue;
+			if (ined[i] < n) return {};
+			res.push_back(ined[i]-n);
 		}
 		return res;
 	}
@@ -166,9 +130,9 @@ int main() {
 	cin >> n >> m;
 	vector<Edge> edges(m);
 	for (int i = 0; i < m; ++i) {
-		cin >> edges[i].src >> edges[i].tar >> edges[i].cost;
-		--edges[i].src;
-		--edges[i].tar;
+		cin >> edges[i].s >> edges[i].t >> edges[i].c;
+		--edges[i].s;
+		--edges[i].t;
 	}
 	
 	/*
@@ -177,8 +141,7 @@ int main() {
 	for (int i = 0; i < n; ++i) edges[i + m] = makeEdge(n, i, INF);
 	m += n;
 
-	DirMST mst;
-	mst.init(n + 1, edges);
+	DirMST mst(n + 1, edges);
 	mst.contract();
 	vector<int> ans = mst.expand(n);
 
@@ -192,8 +155,7 @@ int main() {
 
 	/*
 	// Find cost for all spanning trees (O(m log(n)^2 + n^2))
-	DirMST mst;
-	mst.init(n, edges);
+	DirMST mst(n, edges);
 	mst.contract();
 
 	for (int i = 0; i < n; ++i) {
