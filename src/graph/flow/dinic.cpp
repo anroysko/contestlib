cc6fe5fb0ac0ba03e3c4126837d1705b96ad8424
Antti Roeyskoe <aroysko@paivola.fi>
2019-07-31 13:08:24 +0300

    rewrite dinic

--- src/graph/flow/dinic.cpp
+++ src/graph/flow/dinic.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-typedef long long ll;
+using ll = long long;
 
 // Dinic's algorithm for max flow
 // Source must be node 0, sink must be node n-1
@@ -9,85 +9,82 @@ typedef long long ll;
 // O(V^2 E) in general
 // O(min(V^{2/3}, sqrt{E}) E) in graphs with unit capacities
 // O(sqrt{V} E) in unit networks (e.g. bipartite matching)
-struct Dinic {
-	struct Edge {
-		const int src;
-		const int tar;
-		const ll cap;
-		ll flow;
+class Dinic {
+	private:
+		constexpr static ll INF = 8*(ll)1e18;
+		struct Edge {
+			const int s, t;
+			const ll c; // capacity
+			ll f; // flow
 
-		Edge(int a, int b, ll c, ll f = 0) : src(a), tar(b), cap(c) {
-			flow = f;
-		}
-	};
-	static int getOth(int i, const Edge& ed) {
-		return (i == ed.src ? ed.tar : ed.src);
-	}
-	static ll getCap(int i, const Edge& ed) {
-		return (i == ed.src ? ed.cap - ed.flow : ed.flow);
-	}
-	static void push(int i, Edge& ed, ll am) {
-		ed.flow += (i == ed.src ? am : -am);
-	}
+			Edge(int src, int tar, ll cap = INF, bool dir = 1)
+				: s(src), t(tar), c(dir ? cap : 2*cap), f(dir ? 0 : cap) {}
 
-	vector<Edge> edges;
-	vector<vector<int>> conns; // List of adjanced edges
-	vector<int> dist; // Distance to sink
-	int flow_src; // source
-	int flow_tar; // sink
+			int getOth(int i) { return i == s ? t : s; }
+			ll getCap(int i) { return i == s ? c - f : f; }
+			void push(int i, ll am) { f += (i == s ? am : -am); }
+		};
 
-	bool calcDists() {
-		for (int i = 0; i < dist.size(); ++i) dist[i] = dist.size();
-		dist[flow_tar] = 0;
-		vector<int> que = {flow_tar};
-		for (int j = 0; j < que.size(); ++j) {
-			int i = que[j];
-			for (int ei : conns[i]) {
-				int t = getOth(i, edges[ei]);
-				ll cap = getCap(t, edges[ei]);
-				if (cap > 0 && dist[t] > dist[i]+1) {
-					dist[t] = dist[i]+1;
-					que.push_back(t);
+		const int source, sink, n;
+		vector<Edge> edges;
+		vector<vector<int>> conns;
+		vector<int> dist;
+		vector<bool> done;
+		
+		bool calcDists() {
+			for (int i = 0; i < n; ++i) dist[i] = n;
+			dist[sink] = 0;
+			vector<int> que = {sink};
+			for (int j = 0; j < que.size(); ++j) {
+				int i = que[j];
+				for (auto ei : conns[i]) {
+					int t = edges[ei].getOth(i);
+					if (edges[ei].getCap(t) == 0) continue;
+					if (dist[t] == n) {
+						dist[t] = dist[i] + 1;
+						que.push_back(t);
+					}
 				}
 			}
+			return dist[source] < n;
 		}
-		return dist[flow_src] < dist.size(); // Return true if sink is reachable from source
-	}
-	ll dfsFlow(int i, ll mx = 8*(ll)1e18) {
-		if (i == flow_tar || mx == 0) return mx;
-		ll res = 0; // Amount of flow we end up pushing
-		for (int ei : conns[i]) {
-			int t = getOth(i, edges[ei]);
-			if (dist[t] != dist[i]-1) continue; // edge not in level graph
-			ll cap = getCap(i, edges[ei]);
-			ll pc = dfsFlow(t, min(mx - res, cap));
-			push(i, edges[ei], pc);
-			res += pc;
-		}
-		return res;
-	}
+		ll dfsFlow(int i, ll cap = INF) {
+			if (i == sink) return cap;
+			ll res = 0;
+			for (auto ei : conns[i]) {
+				int t = edges[ei].getOth(i);
+				if (done[t] || dist[t] != dist[i] - 1) continue;
 
-	void init(int n, int src, int sink) {
-		flow_src = src;
-		flow_tar = sink;
-		conns.resize(n);
-		dist.resize(n);
-	}
-	int addEdge(int src, int tar, ll cap, ll flow = 0) {
-		int i = edges.size();
-		edges.emplace_back(src, tar, cap, flow);
-		conns[src].push_back(i);
-		conns[tar].push_back(i);
-		return i; // Returns index of added edge
-	}
-	ll pushFlow() {
-		ll res = 0;
-		while(calcDists()) res += dfsFlow(flow_src);
-		return res;
-	}
-	const Edge& getEdge(int edge_id) const {
-		return edges[edge_id];
-	}
+				ll subcap = min(cap-res, edges[ei].getCap(i));
+				if (subcap == 0) {
+					if (cap == res) return res;
+				} else {
+					ll add = dfsFlow(t, subcap);
+					res += add;
+					edges[ei].push(i, add);
+				}
+			}
+			if (res < cap) done[i] = true;
+			return res;
+		}
+	public:
+		Dinic(int nn, int s, int t)
+			: n(nn), conns(nn), dist(nn), done(nn), source(s), sink(t) {}
+
+		const Edge& getEdge(int j) const { return edges[j]; }
+		void addEdge(int a, int b, ll c = INF, bool dir = 1) {
+			edges.emplace_back(a, b, c, dir);
+			conns[a].push_back(edges.size() - 1);
+			conns[b].push_back(edges.size() - 1);
+		}
+		ll pushFlow() {
+			ll res = 0;
+			while(calcDists()) {
+				for (int i = 0; i < n; ++i) done[i] = false;
+				res += dfsFlow(source);
+			}
+			return res;
+		}
 };
 
 // Example usage
@@ -95,8 +92,7 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 
-	Dinic dinic;
-	dinic.init(n, 0, n-1);
+	Dinic dinic(n, 0, n-1);
 
 	for (int i = 0; i < m; ++i) {
 		int a, b;
@@ -108,10 +104,4 @@ int main() {
 	
 	ll flow = dinic.pushFlow();
 	cout << "Total flow: " << flow << '\n';
-	cout << "Flow per edge: ";
-	for (int i = 0; i < m; ++i) {
-		auto ed = dinic.getEdge(i);
-		cout << dinic.getEdge(i).flow << ' ';
-	}
-	cout << '\n';
 }
---
+++ src/util/tester.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+
+// rand-function that works properly on windows, and is faster than rand
+// https://codeforces.com/blog/entry/61587
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T>
+T rand(T a, T b) {
+	return uniform_int_distribution<T>(a, b)(rng);
+}
+template<class T>
+T rand() {
+	return uniform_int_distribution<T>()(rng);
+}
+
+void genTest(ofstream& fout) {
+	// Generate and output test here
+}
+
+int main() {
+	for (int ti = 0;; ++ti) {
+		cerr << ti << ' ';
+		if (ti % 10 == 9) cerr << endl;
+
+		ofstream fout;
+		fout.open("test.in");
+		
+		/*
+		int t = T;
+		fout << t << '\n';
+		for (int ti = 0; ti < t; ++ti) genTest(fout);
+		*/
+		genTest(fout);
+
+		fout.close();
+
+		bool fail = false;
+		fail |= system("./code < test.in > code.out");
+		fail |= system("./brute < test.in > brute.out");
+		fail |= system("diff code.out brute.out");
+		if (fail) {
+			cout << "BUG FOUND!\n";
+			break;
+		}
+	}
+}
