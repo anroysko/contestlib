9a2d9e97be8dd1883819899ef162b0f7829c96fe
Antti Roeyskoe <aroysko@paivola.fi>
2019-08-03 16:07:19 +0300

    update hld

--- src/graph/tree/hld.cpp
+++ src/graph/tree/hld.cpp
@@ -1,10 +1,8 @@
 class HLD {
 	private:
-		vector<int> par;
-		vector<int> siz;
-		vector<int> cmp;
-		vector<int> ind;
+		vector<int> par, siz, cmp, ind;
 	public:
+		// Assumes the tree is connected, 0 is the root and par[i] < i for all i
 		HLD(const vector<int>& p) : par(p), siz(p.size(), 1), cmp(p.size(), 0), ind(p.size()) {
 			int n = par.size();
 			for (int i = n-1; i > 0; --i) siz[par[i]] += siz[i];
@@ -22,6 +20,7 @@ class HLD {
 				aux[i] += ind[i];
 			}
 		}
+		// Returns intervals corresponding to the path between a and b, not necessarily in order
 		vector<pair<int, int>> path(int a, int b) const {
 			vector<pair<int, int>> res;
 			for (;; b = par[cmp[b]]) {
@@ -33,9 +32,11 @@ class HLD {
 				res.push_back({ind[cmp[b]], ind[b]});
 			}
 		}
+		// Returns interval corresponding to the subtree of node i
 		pair<int, int> subtree(int i) const {
 			return {ind[i], ind[i] + siz[i] - 1};
 		}
+		// Returns LCA of nodes a and b
 		int lca(int a, int b) const {
 			for (;; b = par[cmp[b]]) {
 				if (ind[b] < ind[a]) swap(a, b);
