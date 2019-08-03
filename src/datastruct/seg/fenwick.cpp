6620ea14e1a6258bf06a4ba293b603df2b40d3f4
Antti Roeyskoe <aroysko@paivola.fi>
2019-08-03 16:51:21 +0300

    write fenwick tree

---
+++ src/datastruct/seg/fenwick.cpp
@@ -0,0 +1,18 @@
+class Fenwick {
+	private:
+		vector<ll> val;
+	public:
+		Fenwick(int n) : val(n+1, 0) {}
+		void add(int i, ll v) {
+			for (++i; i < val.size(); i += i & -i) {
+				val[i] += v;
+			}
+		}
+		ll get(int i) {
+			ll res = 0;
+			for (++i; i > 0; i -= i & -i) {
+				res += val[i];
+			}
+			return res;
+		}
+};
