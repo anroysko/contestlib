fa35f45a94e8ba1a08d1ae102593dc963eb9516a
Antti Roeyskoe <aroysko@paivola.fi>
2019-08-05 14:50:27 +0300

    update brute

--- src/datastruct/convhull/lichao/brute.cpp
+++ src/datastruct/convhull/lichao/brute.cpp
@@ -8,6 +8,31 @@ struct Line {
 	ll eval(ll x) const { return a*x + b; }
 };
 
+void solve() {
+	int n, q;
+	cin >> n >> q;
+
+	vector<ll> xs(n);
+	for (int i = 0; i < n; ++i) cin >> xs[i];
+	
+	vector<Line> lines;
+	for (int qi = 0; qi < q; ++qi) {
+		string op;
+		cin >> op;
+		if (op == "add_line") {
+			ll a, b;
+			cin >> a >> b;
+			lines.push_back({a, b});
+		} else if (op == "min_line") {
+			int j;
+			cin >> j;
+			ll res = lines[0].eval(xs[j]);
+			for (int i = 1; i < lines.size(); ++i) res = min(res, lines[i].eval(xs[j]));
+			cout << res << '\n';
+		}
+	}
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
@@ -15,27 +40,6 @@ int main() {
 	int t;
 	cin >> t;
 	for (int ti = 0; ti < t; ++ti) {
-		int n, q;
-		cin >> n >> q;
-
-		vector<ll> xs(n);
-		for (int i = 0; i < n; ++i) cin >> xs[i];
-		
-		vector<Line> lines;
-		for (int qi = 0; qi < q; ++qi) {
-			string op;
-			cin >> op;
-			if (op == "add_line") {
-				ll a, b;
-				cin >> a >> b;
-				lines.push_back({a, b});
-			} else if (op == "min_line") {
-				int j;
-				cin >> j;
-				ll res = lines[0].eval(xs[j]);
-				for (int i = 1; i < lines.size(); ++i) res = min(res, lines[i].eval(xs[j]));
-				cout << res << '\n';
-			}
-		}
+		solve();
 	}
 }
