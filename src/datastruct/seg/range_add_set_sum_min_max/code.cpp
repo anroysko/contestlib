6f22e6d6ce96821ca1368a6e1783658e3413a10e
Antti Roeyskoe <aroysko@paivola.fi>
2019-09-20 15:37:01 +0300

    improve caching in code

--- src/datastruct/seg/range_add_set_sum_min_max/code.cpp
+++ src/datastruct/seg/range_add_set_sum_min_max/code.cpp
@@ -2,67 +2,73 @@
 class SegTree {
 	private:
 		const array<ll, 3> NEUT = {0, 4*(ll)1e18, -4*(ll)1e18};
-		const array<ll, 3> INI = {0, 0, 0};
-		vector<array<ll, 3>> seg;
-		vector<ll> tag, tag_t;
-		int h = 1;
+		struct Row {
+			array<ll, 3> dat = {0, 0, 0}; // initial values
+			ll tag = 0;
+			int tag_t = -1;
 
-		void apply(int t, int i, ll v) {
-			if (t == 0) {
-				seg[i][0] += v * len(i);
-				seg[i][1] += v;
-				seg[i][2] += v;
-				if (i < h) {
-					if (tag_t[i] == -1) tag_t[i] = 0;
-					tag[i] += v;
-				}
-			} else if (t == 1) {
-				seg[i][0] = v * len(i);
-				seg[i][1] = v;
-				seg[i][2] = v;
-				if (i < h) {
-					tag_t[i] = 1;
-					tag[i] = v;
+			void apply(int t, ll v, int len) {
+				if (t == 0) {
+					dat[0] += v * len;
+					dat[1] += v;
+					dat[2] += v;
+
+					if (tag_t == -1) tag_t = 0;
+					tag += v;
+				} else {
+					dat[0] = v * len;
+					dat[1] = v;
+					dat[2] = v;
+
+					tag_t = 1;
+					tag = v;
 				}
 			}
-		}
-		static ll combine(int t, ll a, ll b) {
-			if (t == 0) return a + b;
-			else if (t == 1) return min(a, b);
-			else return max(a, b);
-		}
+			static ll combine(int t, ll a, ll b) {
+				if (t == 0) return a + b;
+				else if (t == 1) return min(a, b);
+				else return max(a, b);
+			}
+		};
 
 		// #################
 		// # DO NOT CHANGE #
 		// #################
 
+		vector<Row> seg;
+		int h = 1;
+
 		// Returns length of interval corresponding to position i
 		int len(int i) { return h >> (31 - __builtin_clz(i)); }
 
 		void push(int i) {
-			apply(tag_t[i], 2*i, tag[i]);
-			apply(tag_t[i], 2*i+1, tag[i]);
-			tag_t[i] = -1;
-			tag[i] = 0;
+			int t = seg[i].tag_t;
+			if (t != -1) {
+				int ln = len(i) >> 1;
+				seg[2*i].apply(t, seg[i].tag, ln);
+				seg[2*i+1].apply(t, seg[i].tag, ln);
+				seg[i].tag_t = -1;
+				seg[i].tag = 0;
+			}
 		}
 		void update(int i) {
-			for (int t = 0; t < INI.size(); ++t) {
-				seg[i][t] = combine(t, seg[2*i][t], seg[2*i+1][t]);
+			for (int t = 0; t < NEUT.size(); ++t) {
+				seg[i].dat[t] = Row::combine(t, seg[2*i].dat[t], seg[2*i+1].dat[t]);
 			}
 		}
 
 		ll recGet(int t, int a, int b, int i, int ia, int ib) {
 			if (ib <= a || b <= ia) return NEUT[t];
-			if (a <= ia && ib <= b) return seg[i][t];
+			if (a <= ia && ib <= b) return seg[i].dat[t];
 			push(i);
 			int im = (ia + ib) >> 1;
-			return combine(t,
+			return Row::combine(t,
 				recGet(t, a, b, 2*i, ia, im),
 				recGet(t, a, b, 2*i+1, im, ib));
 		}
 		void recApply(int t, int a, int b, ll v, int i, int ia, int ib) {
 			if (ib <= a || b <= ia) return;
-			if (a <= ia && ib <= b) apply(t, i, v);
+			if (a <= ia && ib <= b) seg[i].apply(t, v, len(i));
 			else {
 				push(i);
 				int im = (ia + ib) >> 1;
@@ -74,9 +80,7 @@ class SegTree {
 	public:
 		SegTree(int n) {
 			while(h < n) h *= 2;
-			seg.resize(2*h, INI);
-			tag.resize(h, 0);
-			tag_t.resize(h, -1);
+			seg.resize(2*h);
 		}
 		ll rangeSum(int a, int b) { return recGet(0, a, b+1, 1, 0, h); }
 		ll rangeMin(int a, int b) { return recGet(1, a, b+1, 1, 0, h); }
