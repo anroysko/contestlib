41186fee2ea5d2321eb52b50a51178a5ec948778
Antti Roeyskoe <aroysko@paivola.fi>
2019-11-30 03:00:09 +0200

    rename to block cut tree, trim unnecessary stuff

--- src/graph/cond/block_cut/code.cpp
+++ src/graph/cond/block_cut/code.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 void addEdge(int a, int b, vector<vector<int>>& g) {
 	g[a].push_back(b);
 	g[b].push_back(a);
 }
 
-pair<int, int> dfsCD(int i, vector<int>& dep, vector<vector<int>>& ans, const vector<vector<int>>& g) {
+pair<int, int> dfsBC(int i, vector<int>& dep, vector<vector<int>>& ans, const vector<vector<int>>& g) {
 	pair<int, int> res = {-1, g.size()};
 	for (auto t : g[i]) {
 		if (dep[t] == -1) {
 			dep[t] = dep[i] + 1;
-			auto off = dfsCD(t, dep, ans, g);
+			auto off = dfsBC(t, dep, ans, g);
 			if (off.first == -1) addEdge(i, t, ans);
 			else {
 				if (off.second < dep[i]) {
@@ -32,32 +28,13 @@ pair<int, int> dfsCD(int i, vector<int>& dep, vector<vector<int>>& ans, const ve
 	return res;
 }
 
-vector<vector<int>> cycleDecomp(const vector<vector<int>>& g) {
+// Returns a tree s.t. x is on the path between y and x if and only if
+// x is on every path between y and z in the input graph.
+// The tree has size <= |E| + 1. Complexity O(|E|)
+vector<vector<int>> blockCut(const vector<vector<int>>& g) {
 	vector<vector<int>> res(g.size());
 	vector<int> dep(g.size(), -1);
 	dep[0] = 0;
 	dfsCD(0, dep, res, g);
 	return res;
-}
-
-int main() {
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int>> g(n);
-	for (int j = 0; j < m; ++j) {
-		int a, b;
-		cin >> a >> b;
-		--a; --b;
-		g[a].push_back(b);
-		g[b].push_back(a);
-	}
-
-	vector<vector<int>> ans = cycleDecomp(g);
-	int nn = ans.size();
-
-	cout << nn << '\n';
-	for (int i = 0; i < nn; ++i) {
-		cout << i << ": ";
-		for (auto t : ans[i]) cout << t << ' '; cout << '\n';
-	}
 }
