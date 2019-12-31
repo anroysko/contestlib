6792c9343a67c95bad0f614f93cefa1bd6a01520
Antti Roeyskoe <aroysko@paivola.fi>
2019-12-31 18:09:20 +0200

    fix bugs in djikstra and fast prique

--- src/datastruct/basic/fast_prique.cpp
+++ src/datastruct/basic/fast_prique.cpp
@@ -1,14 +1,18 @@
-// Struct for priority queue operations on
-// index set [1, n] with really good constants
+// Struct for priority queue operations on index set [1, n]
 // push(i, v) overwrites value at position i if one already exists
+// decKey is faster but requires that the new key is smaller than the old one
 struct Prique {
 	const ll INF = 4 * (ll)1e18;
 	vector<pair<ll, int>> data;
+	const int n;
 
-	Prique(int n) : data(2*n, {INF, -1}) {}
+	Prique(int siz) : n(siz), data(2*siz, {INF, -1}) { data[0] = {-INF, -1}; }
 	void push(int i, ll v) {
-		int j = i + data.size() / 2;
-		for (data[j] = {v, i}; j > 1; j >>= 1) data[j>>1] = min(data[j], data[j^1]);
+		data[i+n] = {v, (v >= INF ? -1 : i)};
+		for (i += n; i > 1; i >>= 1) data[i>>1] = min(data[i], data[i^1]);
+	}
+	void decKey(int i, ll v) {
+		for (int j = i+n; data[j].first > v; j >>= 1) data[j] = {v, i};
 	}
 	pair<ll, int> pop() {
 		auto res = data[1];
