fcebdefab84c150660dc6dc9565a8f594719517d
Antti Roeyskoe <aroysko@paivola.fi>
2020-01-19 20:28:11 +0200

    trim linear sieve

---
+++ src/numtheory/sieve/code.cpp
@@ -0,0 +1,15 @@
+pair<vector<int>, vector<int>> sieve(int n) {
+	vector<int> div_ind(n+1, -1), primes;
+	for (int i = 2; i <= n; ++i) {
+		if (div_ind[i] == -1) {
+			div_ind[i] = primes.size();
+			primes.push_back(i);
+		}
+		for (int j = 0; j <= div_ind[i]; ++j) {
+			int t = primes[j] * i;
+			if (t > n) break;
+			div_ind[t] = j;
+		}
+	}
+	return {primes, div_ind};
+}
