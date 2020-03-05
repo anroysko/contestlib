0e38b1b63386f6ec4add5a033a085f5c04ae4518
Antti Roeyskoe <aroysko@paivola.fi>
2020-03-05 15:35:02 +0200

    Improve the code for block-cut graph

--- src/graph/cond/block_cut/code.cpp
+++ src/graph/cond/block_cut/code.cpp
@@ -1,41 +1,30 @@
 void addEdge(int a, int b, vector<vector<int>>& g) {
+	if (a < 0 || b < 0) return;
 	g[a].push_back(b);
 	g[b].push_back(a);
 }
 
-pair<int, int> dfsBC(int i, vector<int>& dep, vector<vector<int>>& ans, const vector<vector<int>>& g) {
-	pair<int, int> res = {-1, g.size()};
+pair<int, int> dfsBC(int i, int p, vector<int>& dep, vector<vector<int>>& ans, const vector<vector<int>>& g) {
+	if (dep[i] != -1) return {dep[i], -1};
+	else dep[i] = dep[p] + 1;
+	addEdge(i, i + g.size(), ans);
+
+	int res = dep[i];
 	for (auto t : g[i]) {
-		if (dep[t] == -1) {
-			dep[t] = dep[i] + 1;
-			auto off = dfsBC(t, dep, ans, g);
-			if (off.first == -1) addEdge(i, t, ans);
-			else {
-				if (off.second < dep[i]) {
-					if (res.first != -1) addEdge(res.first, off.first, ans);
-					if (off.second < res.second) res = off;
-				} else addEdge(i, off.first, ans);
-			}
-		} else if (dep[t] < dep[i] - 1) {
-			if (res.first != -1) res.second = min(res.second, dep[t]);
-			else {
-				res = {ans.size(), dep[t]};
-				ans.emplace_back();
-			}
-		}
+		auto sub = dfsBC(t, i, dep, ans, g);
+		res = min(res, sub.first);
+		addEdge(sub.second, t + g.size(), ans);
 	}
-	if (res.first != -1) addEdge(i, res.first, ans);
-	return res;
+	return {res, p + g.size() * (res < dep[p])};
 }
 
 // Given connected undirected graph, returns a tree s.t. x is on the path
 // between y and z in the tree iff x is on every path between y and z in the input graph.
 // A vertex is a cut vertex iff it has degree >= 2 in the tree.
-// The tree has size <= |E| + 1. Complexity O(|E|)
+// The tree has size 2|V|. Complexity O(|E|)
 vector<vector<int>> blockCut(const vector<vector<int>>& g) {
-	vector<vector<int>> res(g.size());
+	vector<vector<int>> res(2 * (int)g.size());
 	vector<int> dep(g.size(), -1);
-	dep[0] = 0;
-	dfsBC(0, dep, res, g);
+	dfsBC(0, 0, dep, res, g);
 	return res;
 }
