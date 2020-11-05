ecf903117baa377272d2af3cb701dd8d95c5ec0f
Antti Roeyskoe <aroysko@paivola.fi>
2020-11-05 23:34:12 +0100

    Fix false comments in fast_prique

--- src/datastruct/basic/fast_prique.cpp
+++ src/datastruct/basic/fast_prique.cpp
@@ -1,4 +1,4 @@
-// Struct for priority queue operations on index set [1, n]
+// Struct for priority queue operations on index set [0, n-1]
 // push(i, v) overwrites value at position i if one already exists
 // decKey is faster but requires that the new key is smaller than the old one
 struct Prique {
@@ -7,6 +7,9 @@ struct Prique {
 	const int n;
 
 	Prique(int siz) : n(siz), data(2*siz, {INF, -1}) { data[0] = {-INF, -1}; }
+	bool empty() const { return data[1].second >= INF; }
+	pair<ll, int> top() const { return data[1]; }
+
 	void push(int i, ll v) {
 		data[i+n] = {v, (v >= INF ? -1 : i)};
 		for (i += n; i > 1; i >>= 1) data[i>>1] = min(data[i], data[i^1]);
