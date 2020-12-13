3be139a59e72975f5f09a90a9694af747fef54af
Antti Roeyskoe <aroysko@paivola.fi>
2020-12-13 17:53:21 +0100

    Change directed MST to use indexes with meldable heat instead of pointers

--- src/graph/dir_mst.cpp
+++ src/graph/dir_mst.cpp
@@ -13,46 +13,50 @@ T rand() {
 }
 
 // Meldable Heap
-struct MH {
-	const int ind;
-	ll val, inc = 0;
-	MH* ch[2] = {nullptr, nullptr};
+class MeldHeap {
+	private:
+		struct Node {
+			ll val, inc = 0;
+			int ch[2] = {-1, -1};
+			Node(ll v) : val(v) {}
+		};
+		vector<Node> nodes;
 
-	MH(int i, ll v) : ind(i), val(v) {}
-	~MH() { delete ch[0]; delete ch[1]; }
+		void push(int i) {
+			add(nodes[i].ch[0], nodes[i].inc);
+			add(nodes[i].ch[1], nodes[i].inc);
+			nodes[i].inc = 0;
+		}
+	public:
+		// Make new node. Returns its index. Indexes go 0, 1, ...
+		int makeNode(ll v) {
+			nodes.emplace_back(v);
+			return (int)nodes.size() - 1;
+		}
+		// Increment all values in heap p by v
+		void add(int i, ll v) {
+			if (i == -1) return;
+			nodes[i].val += v;
+			nodes[i].inc += v;
+		}
+		// Merge heaps a and b
+		int meld(int a, int b) {
+			if (a == -1 || b == -1) return a + b + 1;
+			if (nodes[a].val > nodes[b].val) swap(a, b);
 
-	static void push(MH* p) {
-		add(p->ch[0], p->inc);
-		add(p->ch[1], p->inc);
-		p->inc = 0;
-	}
-
-	// Increment all values in heap p by v
-	static void add(MH* p, ll v) {
-		if (! p) return;
-		p->val += v;
-		p->inc += v;
-	}
-	// Merge heaps a and b
-	static MH* meld(MH* a, MH* b) {
-		if (! a) return b;
-		if (! b) return a;
-		if (a->val > b->val) swap(a, b);
-
-		push(a);
-		int x = rand(0, 1);
-		a->ch[x] = meld(a->ch[x], b);
-		return a;
-	}
-	// Pop minimum element in heap p
-	static pair<int, ll> pop(MH*& p) {
-		pair<int, ll> res = {p->ind, p->val};
-		MH* tmp = p;
-		p = meld(p->ch[0], p->ch[1]);
-		tmp->ch[0] = tmp->ch[1] = nullptr;
-		delete tmp;
-		return res;
-	}
+			push(a);
+			int x = rand(0, 1);
+			nodes[a].ch[x] = meld(nodes[a].ch[x], b);
+			return a;
+		}
+		pair<int, ll> top(int p) const {
+			return {p, nodes[p].val};
+		}
+		pair<int, ll> pop(int& p) {
+			pair<int, ll> res = top(p);
+			p = meld(nodes[p].ch[0], nodes[p].ch[1]);
+			return res;
+		}
 };
 
 struct Edge {
@@ -64,9 +68,9 @@ struct Edge {
 class DirMst {
 	private:
 		const vector<Edge> ed;
-		vector<int> par, cmp, pick;
-		vector<MH*> ins;
-		int n, x = 0; // x: remaining vertex after contractions
+		vector<int> par, cmp, pick, ins;
+		MeldHeap heap;
+		int n, x = 0;
 		
 		void setp(int i, int p) { par[i] = p; cmp[i] = p; }
 		int getc(int i) {
@@ -74,10 +78,10 @@ class DirMst {
 			return cmp[i];
 		}
 		int adv(int i) {
-			while(ins[i]) {
-				int r = getc(ed[ins[i]->ind].s);
+			while(ins[i] != -1) {
+				int r = getc(ed[ins[i]].s);
 				if (r != i) return r;
-				else MH::pop(ins[i]);
+				else heap.pop(ins[i]);
 			}
 			return -1;
 		}
@@ -86,21 +90,20 @@ class DirMst {
 		}
 	public:
 		// INPUT GRAPH MUST BE FULLY CONNECTED!
-		DirMst(int n_, const vector<Edge>& e) : n(n_), ed(e), par(2*n_), cmp(2*n_), pick(2*n_), ins(2*n_, nullptr) {
+		DirMst(int n_, const vector<Edge>& e) : n(n_), ed(e), par(2*n_), cmp(2*n_), pick(2*n_), ins(2*n_, -1) {
 			for (int i = 0; i < 2*n; ++i) setp(i, i);
-			for (int j = 0; j < ed.size(); ++j) ins[ed[j].t] = MH::meld(ins[ed[j].t], new MH(j, ed[j].w));
+			for (int j = 0; j < ed.size(); ++j) ins[ed[j].t] = heap.meld(ins[ed[j].t], heap.makeNode(ed[j].w));
 			for (int j = n; adv(x) != -1; ++j) {
 				for (; pick[x] < j; x = adv(x)) pick[x] = j;
 				for (; x != j; x = getc(ed[pick[x]].s)) {
 					ll w;
-					tie(pick[x], w) = MH::pop(ins[x]);
-					MH::add(ins[x], -w);
-					ins[j] = MH::meld(ins[j], ins[x]);
+					tie(pick[x], w) = heap.pop(ins[x]);
+					heap.add(ins[x], -w);
+					ins[j] = heap.meld(ins[j], ins[x]);
 					setp(x, j);
 				}
 			}
 			for (int& v : cmp) v = -1; // reuse for unpack
-			if (ins[x]) delete ins[x];
 		}
 
 		// Returns the total cost of and the edges in the minimum directed spanning tree rooted at r
