3af954971cce0bdc7d48f23d923578f70dbb209f
Antti Roeyskoe <aroysko@paivola.fi>
2020-12-13 21:09:53 +0100

    Rewrite directed minimum spanning tree to use skew heaps

--- src/graph/dir_mst.cpp
+++ src/graph/dir_mst.cpp
@@ -2,18 +2,8 @@
 using namespace std;
 using ll = long long;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-template<class T>
-T rand(T a, T b) {
-	return uniform_int_distribution<T>(a, b)(rng);
-}
-template<class T>
-T rand() {
-	return uniform_int_distribution<T>()(rng);
-}
-
-// Meldable Heap
-class MeldHeap {
+// Skew Heap
+class SkewHeap {
 	private:
 		struct Node {
 			ll val, inc = 0;
@@ -21,12 +11,6 @@ class MeldHeap {
 			Node(ll v) : val(v) {}
 		};
 		vector<Node> nodes;
-
-		void push(int i) {
-			add(nodes[i].ch[0], nodes[i].inc);
-			add(nodes[i].ch[1], nodes[i].inc);
-			nodes[i].inc = 0;
-		}
 	public:
 		// Make new node. Returns its index. Indexes go 0, 1, ...
 		int makeNode(ll v) {
@@ -40,23 +24,21 @@ class MeldHeap {
 			nodes[i].inc += v;
 		}
 		// Merge heaps a and b
-		int meld(int a, int b) {
+		int merge(int a, int b) {
 			if (a == -1 || b == -1) return a + b + 1;
 			if (nodes[a].val > nodes[b].val) swap(a, b);
-
-			push(a);
-			int x = rand(0, 1);
-			nodes[a].ch[x] = meld(nodes[a].ch[x], b);
+			if (nodes[a].inc) {
+				add(nodes[a].ch[0], nodes[a].inc);
+				add(nodes[a].ch[1], nodes[a].inc);
+				nodes[a].inc = 0;
+			}
+			swap(nodes[a].ch[0], nodes[a].ch[1]);
+			nodes[a].ch[0] = merge(nodes[a].ch[0], b);
 			return a;
 		}
-		pair<int, ll> top(int p) const {
-			return {p, nodes[p].val};
-		}
-		pair<int, ll> pop(int& p) {
-			pair<int, ll> res = top(p);
-			p = meld(nodes[p].ch[0], nodes[p].ch[1]);
-			return res;
-		}
+		// "top" and pop -functions
+		const Node& operator[](int i) const { return nodes[i]; }
+		void pop(int& p) { p = merge(nodes[p].ch[0], nodes[p].ch[1]); }
 };
 
 struct Edge {
@@ -68,53 +50,45 @@ struct Edge {
 class DirMst {
 	private:
 		const vector<Edge> ed;
-		vector<int> par, cmp, pick, ins;
-		MeldHeap heap;
+		vector<int> par, cmp, ins, vis;
+		SkewHeap heap;
 		int n, x = 0;
 		
 		void setp(int i, int p) { par[i] = p; cmp[i] = p; }
+		int nxt(int x) { return getc(ed[ins[x]].s); }
+
 		int getc(int i) {
 			if (cmp[i] != i) cmp[i] = getc(cmp[i]);
 			return cmp[i];
 		}
-		int adv(int i) {
-			while(ins[i] != -1) {
-				int r = getc(ed[ins[i]].s);
-				if (r != i) return r;
-				else heap.pop(ins[i]);
-			}
-			return -1;
-		}
 		void setDI(int i, int t) {
-			for (; cmp[i] < t; i = par[i]) cmp[i] = t;
+			for (; vis[i] < t; i = par[i]) vis[i] = t;
 		}
 	public:
-		// INPUT GRAPH MUST BE FULLY CONNECTED!
-		DirMst(int n_, const vector<Edge>& e) : n(n_), ed(e), par(2*n_), cmp(2*n_), pick(2*n_), ins(2*n_, -1) {
+		// INPUT GRAPH MUST BE FULLY CONNECTED, AND CONTAIN NO SELF-LOOPS
+		DirMst(int n_, const vector<Edge>& e) : n(n_), ed(e), par(2*n_), cmp(2*n_), ins(2*n_, -1), vis(2*n_) {
 			for (int i = 0; i < 2*n; ++i) setp(i, i);
-			for (int j = 0; j < ed.size(); ++j) ins[ed[j].t] = heap.meld(ins[ed[j].t], heap.makeNode(ed[j].w));
-			for (int j = n; adv(x) != -1; ++j) {
-				for (; pick[x] < j; x = adv(x)) pick[x] = j;
-				for (; x != j; x = getc(ed[pick[x]].s)) {
-					ll w;
-					tie(pick[x], w) = heap.pop(ins[x]);
-					heap.add(ins[x], -w);
-					ins[j] = heap.meld(ins[j], ins[x]);
+			for (int j = 0; j < ed.size(); ++j) ins[ed[j].t] = heap.merge(ins[ed[j].t], heap.makeNode(ed[j].w));
+			for (int j = n; ins[x] != -1; ++j) {
+				for (; !vis[x]; x = nxt(x)) vis[x] = 1;
+				for (; x != j; x = nxt(x)) {
+					heap.add(ins[x], -heap[ins[x]].val);
+					ins[j] = heap.merge(ins[j], ins[x]);
 					setp(x, j);
 				}
+				while(ins[x] != -1 && nxt(x) == x) heap.pop(ins[x]);
 			}
-			for (int& v : cmp) v = -1; // reuse for unpack
 		}
 
 		// Returns the total cost of and the edges in the minimum directed spanning tree rooted at r
 		pair<ll, vector<int>> unpack(int r) {
-			setDI(r, cmp[x] + 1);
+			setDI(r, vis[x] + 2);
 			pair<ll, vector<int>> res;
 			for (int i = x; i >= 0; --i) {
-				if (cmp[i] < cmp[x]) {
-					res.second.push_back(pick[i]);
-					res.first += ed[pick[i]].w;
-					setDI(ed[pick[i]].t, cmp[x]);
+				if (vis[i] < vis[x]) {
+					res.second.push_back(ins[i]);
+					res.first += ed[ins[i]].w;
+					setDI(ed[ins[i]].t, vis[x]);
 				}
 			}
 			return res;
@@ -134,7 +108,10 @@ int main() {
 		cin >> a >> b >> w;
 		edges.emplace_back(a, b, w);
 	}
-	for (int i = 0; i < n; ++i) edges.emplace_back(i, r, 0); // make sure the graph is fully connected
+
+	for (int i = 0; i < n; ++i) {
+		if (i != r) edges.emplace_back(i, r, 0); // make sure the graph is fully connected
+	}
 
 	DirMst mst(n, edges);
 	auto res = mst.unpack(r);
