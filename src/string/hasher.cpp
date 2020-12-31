9e98735ca7924abd445b0324371bb89fee8e564e
Antti Roeyskoe <aroysko@paivola.fi>
2020-12-31 11:13:28 +0100

    Fix hasher

--- src/string/hasher.cpp
+++ src/string/hasher.cpp
@@ -10,13 +10,13 @@ template<class T>
 T rand() {
 	return uniform_int_distribution<T>()(rng);
 }
-
 template<int K, int H, class T = string> // Number of hashes to use, max string length (at least 2). Probability of collision <= (H / 4e9)^K
 class Hasher {
 	private:
 		const static uint P = (uint)4e9 + 7;
 		const static array<array<uint, K>, H> mults;
 		vector<array<uint, K>> hashes;
+		static uint mSub(uint a, uint b) { return (a >= b ? a - b : a + P - b); }
 	public:
 		Hasher(const T& s) : hashes(s.size() + 1) {
 			for (int j = 0; j < K; ++j) hashes[0][j] = 1;
@@ -26,12 +26,10 @@ class Hasher {
 		}
 		// Hash for substring s[a, b], 0 <= a <= b + 1 <= |s|
 		array<uint, K+1> operator()(int a, int b) const {
+			int len = b-a+1;
 			array<uint, K+1> res;
-			for (int j = 0; j < K; ++j) {
-				res[j] = (hashes[b+1][j] - hashes[a][j] * (ull)mults[b-a+1][j]) % P;
-				if (res[j] < 0) res[j] += P;
-			}
-			res[K] = b-a+1;
+			for (int j = 0; j < K; ++j) res[j] = mSub(hashes[b+1][j], hashes[a][j] * (ull)mults[len][j] % P);
+			res[K] = len;
 			return res;
 		}
 };
@@ -41,8 +39,8 @@ const array<array<uint, K>, H> Hasher<K, H, T>::mults = [](){
 	for (int j = 0; j < K; ++j) res[0][j] = 1;
 	for (int j = 0; j < K; ++j) res[1][j] = rand(1u, P - 1);
 	for (int i = 2; i < H; ++i) {
-		for (int j = 0; j < K; ++j) res[i][j] = res[i-1][j] * (ull)res[1][j] % P;
+		for (int j = 0; j < K; ++j) res[i][j] = res[i-1][j] * (ull)res[1][j] % Hasher<K, H, T>::P;
 	}
 	return res;
 }();
-using HS = Hasher<3, (int)1e6>; // Probability of collision ~ 1e-11, set length!
+using HS = Hasher<3, (int)2e6>; // Probability of collision ~ 1e-10, set length!
