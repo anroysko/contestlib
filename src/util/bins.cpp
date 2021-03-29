a820b8b2064e292ccfbbfb746170031c652b5ad8
Antti Roeyskoe <aroysko@paivola.fi>
2021-03-29 14:06:12 +0200

    Move binary search to its own function

---
+++ src/util/bins.cpp
@@ -0,0 +1,12 @@
+// returns number of elements strictly smaller than v in vec
+template<class T>
+int bins(const vector<T>& vec, T v) {
+	int low = 0;
+	int high = vec.size();
+	while(low != high) {
+		int mid = (low + high) >> 1;
+		if (vec[mid] < v) low = mid + 1;
+		else high = mid;
+	}
+	return low;
+}
