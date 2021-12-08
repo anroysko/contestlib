0fc404ffb506a7d1caf256bc15fe1d3c28ed4a41
Antti Roeyskoe <aroysko@paivola.fi>
2021-12-08 23:05:02 +0100

    Rewrite binary span, UNTESTED

--- src/matrix/binary_span.cpp
+++ src/matrix/binary_span.cpp
@@ -1,38 +1,27 @@
-// Returns set bits in a bitset
-template<class T>
-vector<int> getBits(const T& bts) {
-	vector<int> res;
-	for (int j = bts._Find_first(); j < bts.size(); j = bts._Find_next(j)) res.push_back(j);
-	return res;
-}
-
 // Maintains the span of binary vectors
 template<int M>
 class BinarySpan {
 	private:
 		vector<pair<bitset<M>, bitset<M>>> vecs;
-		
-		pair<int, bitset<M>> tryAdd(bitset<M> bts, bool insert) {
-			bitset<M> ind;
-			for (int i = 0, j = 0; j < M; ++j) {
-				if (i < vecs.size() && vecs[i].first[j]) {
-					if (bts[j]) {
-						bts ^= vecs[i].first;
-						ind ^= vecs[i].second;
-					}
-					++i;
-				} else if (bts[j]) {
-					if (insert) {
-						ind[(int)vecs.size()] = 1;
-						vecs.insert(vecs.begin() + i, {bts, ind});
-					}
-					return {(int)vecs.size() - insert, ind};
-				}
-			}
-			return {-1, ind};
-		}
 	public:
+		// Returns {\min_{ind}, \argmin_{ind}} of \xor_{i : ind[i] = 1} vec[i] for vectors i added to the basis
+		pair<bitset<M>, bitset<M>> reduce(bitset<M> vec) {
+			bitset<M> ind;
+			for (int i = 0; i < vecs.size(); ++i) {
+				bitset<M> nxt = vec ^ vecs[i].first;
+				if (nxt < vec) { vec = nxt; ind ^= inds[i].second; }
+			}
+			return {vec, ind};
+		}
+		// Tries to add "vec" to the basis. Returns -1 if it already is in the basis, otherwise the vector's index in the basis
+		int add(bitset<M> vec) {
+			auto pr = reduce(vec);
+			int res = (pr.first.any() ? (int)vecs.size() : -1);
+			if (res != -1) {
+				pr.second[res] = 1;
+				vecs.emplace_back(pr);
+			}
+			return res;
+		}
 		int rank() const { return vecs.size(); }
-		int addVec(bitset<M> bts) { return tryAdd(bts, 1).first; }
-		pair<bool, vector<int>> inSpan(bitset<M> bts) { auto tmp = tryAdd(bts, 0); return {tmp.first == -1, getBits(tmp.second)}; }
 };
